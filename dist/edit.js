8556e26a4568ee49047a779564db1af48e949983
reach2arunprakash <reach2arunprakash@gmail.com>
2016-06-06 07:04:53 +0530

    two way data-binding in edit.html

--- dist/edit.js
+++ dist/edit.js
@@ -1,12 +1,43 @@
+function JSONModal() {
+	
+	this.Modal_data = {};
+	this.setValue=function setValue(key,val){
+	this.Modal_data[key] = val;
+        
+	}
+	
+	
+	this.getValue=function getValue(key){
+	return this.Modal_data[key];
+        
+	}
+	
+	this.setJSONModal = function setJSONModal(data){
+		this.Modal_data = data;	
+	}
+	
+	this.getJSONModal = function getJSONModal(data){
+		return this.Modal_data;	
+	}
+	
+}
 
 
+$(document).ready(function(){
+
+var objJSON = new JSONModal;
 
 $.getJSON( "sample.json", function( data ) {
   var items = [];
+   objJSON.setJSONModal(data);
+
   $.each( data, function( key, val ) {
-	  var start = "<li><span class=\"test display\">"
-	  var end = "</span><input type=\"text\" class=\"edit\" style=\"display:none\"/></li>"
-    items.push( start + val + end );
+	  var start = "<li><span class='display'>";
+	  var end = "</span><input type='text' class='edit' style='display:none'  data-key ='";
+	  var endli = "'>";
+	  var li_elem = start + val + end + key + endli ;
+	  console.log(li_elem);
+    items.push( li_elem);
   });
  
   $( "<ul/>", {
@@ -16,12 +47,26 @@ $.getJSON( "sample.json", function( data ) {
 });
 
 
+
+
+
+
 $('body').on('click', 'span.display', function() {
         $(this).hide().siblings(".edit").show().val($(this).text()).focus();
 });
 
 
 $('body').on('focusout', 'input.edit', function() {
-        $(this).hide().siblings(".display").show().text($(this).val());
+        var key = $(this).data("key");
+		var val = $(this).val();
+		
+		
+		$(this).hide().siblings(".display").show().text(val);
+		alert(key);
+		objJSON.setValue(key,val);
+		alert(objJSON.getValue(key));
 });
+
+});
+
 
